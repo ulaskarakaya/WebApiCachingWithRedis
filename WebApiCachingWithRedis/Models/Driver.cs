ae21edf9d32fe3ce8b1d1fe03f09c4f10bef7780
Ulaş Karakaya <Ulas.Karakaya@gelecekvarlik.com.tr>
2023-03-03 16:21:14 +0300

    db connection

---
+++ WebApiCachingWithRedis/Models/Driver.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApiCachingWithRedis.Models
+{
+    public class Driver
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DriveNb { get; set; }
+    }
+}
--- WebApiCachingWithRedis/Program.cs
+++ WebApiCachingWithRedis/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using WebApiCachingWithRedis.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddEntityFrameworkNpgsql()
+    .AddDbContext<AppDbContext>(opt =>
+        opt.UseNpgsql(builder.Configuration.GetConnectionString("SampleDbConnection")));
 
 var app = builder.Build();
 
